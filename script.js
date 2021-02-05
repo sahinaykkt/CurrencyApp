2fbeca06b6d5d038552e026c358d45e1e18b134c
Levent007 <levent.mehmed@student.kdg.be>
2021-02-05 01:00:01 +0100

    Code efficient, ES6, changes on html and css (responsive)

--- script.js
+++ script.js
@@ -1,62 +1,57 @@
 function createCurrencyElements(elements, root, inputName){
+    const currencyKeyDiv   = document.createElement("div");
+    const currencyDropdown = document.createElement("select");
+
     for(let i =0; i< elements.length; i++){
-        const currencyKeyDiv   = document.createElement("div");
-        const currencyKeyInput = document.createElement("input");
-        currencyKeyInput.setAttribute("type", "radio");
-        currencyKeyInput.setAttribute("name", inputName);
-        currencyKeyInput.setAttribute("id", inputName + elements[i]);
-        currencyKeyInput.setAttribute("value", elements[i]);
-
-        const currencyKeyLabel = document.createElement("label");
-        currencyKeyLabel.setAttribute("for", inputName + elements[i]);
-        currencyKeyLabel.textContent = elements[i];
-
-        currencyKeyDiv.appendChild(currencyKeyInput);
-        currencyKeyDiv.appendChild(currencyKeyLabel);
-        root.appendChild(currencyKeyDiv);
+        const currencyOption = document.createElement("option");
+            currencyOption.value = elements[i];
+            currencyOption.innerText = currencyOption.value;
+            currencyOption.name = inputName;
+            currencyOption.id = inputName + elements[i];
+        currencyDropdown.appendChild(currencyOption);
     }
-}
 
+    currencyKeyDiv.appendChild(currencyDropdown);
+    root.appendChild(currencyKeyDiv);
+}
 
 //from
 const parentEl = document.querySelector("#currency-box-from");
 const fromInputName = "currency_from";
 
-
 // to
 const parentToEl = document.querySelector("#currency-box-to");
 const toInputName = "currency_to";
 
-const getBaseData = async function(fromTarget){
+const getBaseData = async (fromTarget) => {
     const response =  await fetch(`https://api.exchangeratesapi.io/latest?base=${fromTarget}`);
     const responseJSON = await response.json();
     return responseJSON.rates;
 };
 
-const getData = async function(){
+const getData = async () => {
     const response =  await fetch("https://api.exchangeratesapi.io/latest?base=USD");
     const responseJSON = await response.json();
-    const allCurrencies = Object.keys(responseJSON.rates);
-    return allCurrencies;
+    return Object.keys(responseJSON.rates);
 };
 
-const calculateButton = document.querySelector("#calculate-button");
-calculateButton.addEventListener("click", function(){
-    // kimden ceviriyourz
-    const fromTarget = document.querySelector("input[name='currency_from']:checked").value;
-    // kime ceviriyoruz
-    const toTarget   = document.querySelector("input[name='currency_to']:checked").value;
-    // amountu alalim
-    const amount     = document.querySelector("input[name='amount']").value;
+document.querySelector("#calculate-button")
+        .addEventListener("click", () => {
+                const targets = document.querySelectorAll("select");
+                // kimden ceviriyourz
+                const fromTarget = targets[0].value;
+                // kime ceviriyoruz
+                const toTarget = targets[1].value;
+                // amountu alalim
+                const amount = document.getElementById("amount").value;
 
-    getBaseData(fromTarget, toTarget, amount).then((rates) => {
-        const resultForOne = rates[toTarget];
-        const result = amount * resultForOne;
-        const currencyResult = document.querySelector("#currency-result");
-        currencyResult.innerHTML = amount + " " + fromTarget + " = " + result + " " + toTarget;
-    });
+                getBaseData(fromTarget, toTarget, amount).then((rates) => {
+                    const resultForOne = rates[toTarget];
+                    const result = amount * resultForOne;
+                    const currencyResult = document.querySelector("#currency-result");
+                    currencyResult.innerHTML = amount + " " + fromTarget + " = " + result + " " + toTarget;
+                });
 });
-
 
 getData().then((allCurrencies) => {
     createCurrencyElements(allCurrencies, parentEl, fromInputName);
